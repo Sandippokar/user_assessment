fe2fcb5f39089ad5c02dd8583b8a5304717950b9
Sandip <sandip.pokar2724@gmail.com>
2023-03-05 20:03:16 +0530

    user assessment in react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Loader from "./Loader";
+import User from "./User";
+import { Col, Row } from "antd";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try{
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      if (res.status === 200) {
+        setUsers(res.data);
+      } else {
+        console.log("Data Not Found!");
+      }
+    }catch(error){
+      console.log(Object.keys(error), error.message);
+    }
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((entry) => entry.id !== id));
+  };
+
+  const updateUserData = (id, value) => {
+    setUsers(
+      users.map((user) => {
+        if (user.id === id) {
+          return { ...user, ...value };
+        } 
+        return user;  
+      })
+    );
+  };
+
+  if (users.length === 0) {
+    return <Loader />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      {users.map((user) => (
+        <Col xs={24} sm={24} md={8} lg={8} xl={6} key={user.username}>
+          <User user={user} updateUserData={updateUserData} deleteUser={deleteUser}  />
+        </Col>
+      ))}
+    </Row>
   );
-}
+};
 
 export default App;
---
+++ src/User.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import style from "./User.module.css";
+import { Card, Modal, Form, Input } from "antd";
+import {
+  HeartFilled,
+  HeartOutlined,
+  EditOutlined,
+  DeleteFilled,
+  MailOutlined,
+  PhoneOutlined,
+  GlobalOutlined,
+} from "@ant-design/icons";
+
+const layout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+  },
+};
+
+const User = ({ user, updateUserData, deleteUser }) => {
+  const [form] = Form.useForm();
+  const [showModal, setShowModal] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
+  const handleOk = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        updateUserData(user.id, values);
+        handleCancel();
+      })
+      .catch((info) => {
+        console.log("Validate Failed:", info);
+      });
+  };
+
+  return (
+    <>
+      <Modal
+        title="User Information Modal"
+        open={showModal}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+          form={form}
+          {...layout}
+          name="form_in_modal"
+          style={{ maxWidth: 600 }}
+          validateMessages={validateMessages}
+          initialValues={user}
+        >
+          <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ type: "email", required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="phone" label="Phone" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="website"
+            label="Website"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Card
+        cover={
+          <div className={style.imageDiv}>
+            <img
+              src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+              alt="User Avatar"
+              className={style.image}
+            />
+          </div>
+        }
+        style={{ margin: 15 }}
+        actions={[
+          liked ? (
+            <HeartFilled className={style.likeIcon} onClick={handleLike} />
+          ) : (
+            <HeartOutlined className={style.likeIcon} onClick={handleLike} />
+          ),
+
+          <EditOutlined className={style.icon} onClick={openModal} />,
+
+          <DeleteFilled className={style.icon} onClick={() => {deleteUser(user.id)}} />,
+        ]}
+      >
+        <h3 className={style.userName}>{user.name}</h3>
+        <div className={style.userInfo}>
+          <MailOutlined className={style.icon} />
+          <p className={style.detail}>{user.email}</p>
+        </div>
+        <div className={style.userInfo}>
+          <PhoneOutlined className={style.icon} />
+          <p className={style.detail}>{user.phone}</p>
+        </div>
+        <div className={style.userInfo}>
+          <GlobalOutlined className={style.icon} />
+          <p className={style.detail} style={{ cursor: "pointer" }}>
+            http://{user.website}
+          </p>
+        </div>
+      </Card>
+    </>
+  );
+};
+
+export default User;
